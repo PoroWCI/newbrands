af49b8eda39d0d9e5ed8413ccb80520a6f02d2c1
codingporo <62948870+PoroWCI@users.noreply.github.com>
2021-01-26 01:35:12 +0100

    using now css modules to use cleaner code

--- src/components/contactContainer.js
+++ src/components/contactContainer.js
@@ -1,11 +1,12 @@
 import {useTranslation} from "react-i18next";
 import attachment from '../assets/img/attachment.svg';
+import classes from '../assets/contact.module.css'
 
 function ContactContainer() {
     const {t, i18n} = useTranslation('common');
     return (
-        <div id="contact" className="contentContainer margedContainer rowAlign">
-            <div className="left-column-contact">
+        <div id="contact" className={`${classes.rowAlign} ${classes.contentContainer} ${classes.margedContainer}`}>
+            <div className={classes.leftColumnContact}>
             <h1>{t('contact.title')}</h1>
             <p>{t('contact.subtitle')}</p>
             <form>
@@ -15,10 +16,10 @@ function ContactContainer() {
                 <input type="mail" placeholder="johndoe@gmail.com" id="mail" />
                 <label htmlFor="message">{t('contact.messageInputLabel')}</label>
                 <textarea placeholder={t('contact.messageInput')} id="message" />
-                <div className="sendContainer">
-                    <button className="btn blueBtn rounded">{t('contact.sendBtn')}</button>
+                <div className={classes.sendContainer}>
+                    <button className={`${classes.btn} ${classes.blueBtn} ${classes.rounded}`}>{t('contact.sendBtn')}</button>
                     <label>{t('contact.policy')}<a href="#">{t('contact.policyLink')}</a></label>
-                    <span className="attachment" ><img src={attachment} alt="logo" /> {t('contact.file')}</span>
+                    <span className={classes.attachment} ><img src={attachment} alt="logo" /> {t('contact.file')}</span>
                 </div>
             </form>
             </div>
--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import { HamburgerSpring } from 'react-animated-burgers';
 import React, { useState, useCallback } from 'react';
 import {useTranslation} from "react-i18next";
 import { Route } from 'react-router-dom';
-import { css, cx } from '@emotion/css'
-
+import classes from '../assets/header.module.css'
+ 
 function Header() {
   const {t, i18n} = useTranslation('common');
   const [isActive, setIsActive] = useState(false);
@@ -13,68 +13,23 @@ function Header() {
     [],
   )
   return (
-      <header className={header}>
+      <header className={classes.header}>
         <img src={logo} alt="logo" />
-        <ul className={[cx({ [openMobile]: isActive }, {menu})]}>
+        <ul className={isActive === true ? classes.openMobile : null}>
           <Route render={({ history }) => (
-            <li className={menuElement} onClick={() => history.push('/')}>
-              <a className={link} href="#">{t('header.home')}</a>
+            <li className={classes.menuElement} onClick={() => history.push('/')}>
+              <a className={classes.link} href="#">{t('header.home')}</a>
             </li>
           )}/>
           <Route render={({ history }) => (
-            <li className={menuElement} onClick={() => history.push('/contact')}>
-              <a className={link} href="#">{t('header.contact')}</a>
+            <li className={classes.menuElement} onClick={() => history.push('/contact')}>
+              <a className={classes.link} href="#">{t('header.contact')}</a>
             </li>
           )}/>
         </ul>
-        <HamburgerSpring className={burgerMenu} isActive={isActive} toggleButton={toggleButton} buttonColor="transparent" barColor="#333333" buttonWidth={20} />
+        <HamburgerSpring className={classes.burgerMenu} isActive={isActive} toggleButton={toggleButton} buttonColor="transparent" barColor="#333333" buttonWidth={20} />
       </header>
   );
 }
-
-const header = css`
-  display: "flex",
-  flexDirection: "row",
-  width: "95%",
-  alignItems: "center",
-  margin: "auto",
-  height: "8vh"
-`
-
-const menu = css `
-  listStyle: "none",
-  marginLeft: "auto"
-`
-
-const menuElement = css`
-  float: "left",
-  margin: "20px",
-  &::after{
-    transition: "all 0.2s ease-in-out";
-    fontWeight: "500";
-    content: "";
-    display: "block";
-    height: "1px";
-    width: "0";
-    margin: "auto";
-    marginTop: "5px";
-  }
-`
-
-const burgerMenu = css`
-  marginLeft: "auto",
-  outline: "none",
-  display: "none"
-`
-
-const link = css`
-textDecoration: "none",
-color: "#333333"
-`
-const openMobile = css`
-  visibility: visible;
-  opacity: 1;
-  transition: opacity 0.3s, visibility 0.3s;
-`
 
 export default Header;
