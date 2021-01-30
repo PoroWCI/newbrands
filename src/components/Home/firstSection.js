f598346422365615c9f7a12f90c7c67a5cb255d8
Poro <poro@poros-macbook-pro.home>
2021-01-30 19:08:01 +0100

    Introducing backend, saving contact form to contact table & newsletter

--- src/components/Home/firstSection.js
+++ src/components/Home/firstSection.js
@@ -1,54 +1,78 @@
-import working from '../../assets/img/working.svg'
-import classes from '../../assets/firstSection.module.css'
+import working from "../../assets/img/working.svg";
+import classes from "../../assets/firstSection.module.css";
+import { useState } from "react";
+import axios from "axios";
 
-function firstSection() {
-    return (
-        <div className={classes.container}>
-            <div className={classes.leftColumn}>
-              <h1 className={classes.h1}>
-                Optimisons les chaînes de valeur de l’industrie textile
-                française
-              </h1>
-              <p className={classes.lightText}>
-                NewBrands permet de simplifier et réduire les coûts de la chaîne
-                de production Made In France en sélectionnant pour vous les
-                acteurs les plus adaptés à vos projets de productions.
-              </p>
-              <div>
+function FirstSection() {
+  const [data, setData] = useState({ mail: "" });
+  const handleSubmit = (e) => {
+    console.log("submit");
+    e.preventDefault();
+    console.log(data);
+    axios
+      .post("http://localhost:4000/newsletter", data)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.leftColumn}>
+        <h1 className={classes.h1}>
+          Optimisons les chaînes de valeur de l’industrie textile française
+        </h1>
+        <p className={classes.lightText}>
+          NewBrands permet de simplifier et réduire les coûts de la chaîne de
+          production Made In France en sélectionnant pour vous les acteurs les
+          plus adaptés à vos projets de productions.
+        </p>
+        <div>
+          <div>
+            <div className={classes.dottedHr}></div>
+            <form onSubmit={handleSubmit}>
+              <div className={classes.inputDiv}>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Entrez votre adresse e-mail"
+                  required
+                  className={classes.mailInput}
+                  id="mail"
+                  onChange={handleChange}
+                  value={data.mail}
+                />
                 <div>
-                    <div className={classes.dottedHr}></div>
-                  <form>
-                    <div className={classes.inputDiv}>
-                      <input
-                        type="email"
-                        name="email"
-                        placeholder="Entrez votre adresse e-mail"
-                        required
-                        className={classes.mailInput}
-                      />
-                      <div>
-                        <button type="submit" name="submit" className={classes.btn}>
-                          Être prévenu
-                          <i></i>
-                        </button>
-                      </div>
-                    </div>
-                  </form>
+                  <button type="submit" name="submit" className={classes.btn}>
+                    Être prévenu
+                  </button>
                 </div>
               </div>
-              <div className={classes.noSpam}>
-                <div>
-                  <span className={classes.lightText}>
-                    Nous vous préviendrons par e-mail de la disponibilité de
-                    notre plateforme gratuitement et sans engagement.{" "}
-                    <span className={classes.boldText}>Promis, sans spam.</span>
-                  </span>
-                </div>
-              </div>
-            </div>
-            <img src={working} alt="Working" />
+            </form>
           </div>
-    )
+        </div>
+        <div className={classes.noSpam}>
+          <div>
+            <span className={classes.lightText}>
+              Nous vous préviendrons par e-mail de la disponibilité de notre
+              plateforme gratuitement et sans engagement.{" "}
+              <span className={classes.boldText}>Promis, sans spam.</span>
+            </span>
+          </div>
+        </div>
+      </div>
+      <img src={working} alt="Working" />
+    </div>
+  );
 }
 
-export default firstSection
+export default FirstSection;
--- src/components/contactContainer.js
+++ src/components/contactContainer.js
@@ -1,38 +1,97 @@
-import {useTranslation} from "react-i18next";
-import attachment from '../assets/img/attachment.svg';
-import classes from '../assets/contact.module.css'
+import { useTranslation } from "react-i18next";
+import attachment from "../assets/img/attachment.svg";
+import classes from "../assets/contact.module.css";
+import { useState } from "react";
+import axios from "axios";
 
 function ContactContainer() {
-    const {t, i18n} = useTranslation('common');
-    return (
-        <div id="contact" className={`${classes.rowAlign} ${classes.contentContainer} ${classes.margedContainer}`}>
-            <div className={classes.leftColumnContact}>
-            <h1 className={classes.h1}>{t('contact.title')}</h1>
-            <p>{t('contact.subtitle')}</p>
-            <form className={classes.form}>
-                <label htmlFor="name">{t('contact.nameInputLabel')}</label>
-                <input className={classes.input} type="text" placeholder="John Doe" id="name" />
-                <label htmlFor="mail">{t('contact.mailInputLabel')}</label>
-                <input className={classes.input} type="mail" placeholder="johndoe@gmail.com" id="mail" />
-                <label htmlFor="message">{t('contact.messageInputLabel')}</label>
-                <textarea className={classes.textarea} placeholder={t('contact.messageInput')} id="message" />
-                <div className={classes.sendContainer}>
-                    <button className={`${classes.btn} ${classes.blueBtn} ${classes.rounded}`}>{t('contact.sendBtn')}</button>
-                    <label>{t('contact.policy')}<a href="#">{t('contact.policyLink')}</a></label>
-                    <span className={classes.attachment} ><img src={attachment} alt="logo" /> {t('contact.file')}</span>
-                </div>
-            </form>
-            </div>
-            <div>
-                <h2 className={classes.h2}>{t('contact.sidePanel.headOffice')}</h2>
-                <p>10 Place Vendôme, 75001 Paris, France</p>
-                <br />
-                <h2 className={classes.h2}>{t('contact.sidePanel.contactUs')}</h2>
-                <p>+33 01 32 66 62 61</p>
-                <p>hello@newbrands.fr</p>
-            </div>
-        </div>
-    );
+  const [data, setData] = useState({ name: "", mail: "", message: "" });
+  const handleSubmit = (e) => {
+    console.log("submit");
+    e.preventDefault();
+    console.log(data);
+    axios
+      .post("http://localhost:4000/contact", data)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleChange = (event) => {
+    setData({
+      ...data,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const { t, i18n } = useTranslation("common");
+  return (
+    <div
+      id="contact"
+      className={`${classes.rowAlign} ${classes.contentContainer} ${classes.margedContainer}`}
+    >
+      <div className={classes.leftColumnContact}>
+        <h1 className={classes.h1}>{t("contact.title")}</h1>
+        <p>{t("contact.subtitle")}</p>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <label htmlFor="name">{t("contact.nameInputLabel")}</label>
+          <input
+            className={classes.input}
+            type="text"
+            placeholder="John Doe"
+            id="name"
+            value={data.name}
+            onChange={handleChange}
+            required
+          />
+          <label htmlFor="mail">{t("contact.mailInputLabel")}</label>
+          <input
+            className={classes.input}
+            type="mail"
+            placeholder="johndoe@gmail.com"
+            id="mail"
+            onChange={handleChange}
+            value={data.mail}
+            required
+          />
+          <label htmlFor="message">{t("contact.messageInputLabel")}</label>
+          <textarea
+            className={classes.textarea}
+            placeholder={t("contact.messageInput")}
+            id="message"
+            value={data.message}
+            onChange={handleChange}
+            required
+          />
+          <div className={classes.sendContainer}>
+            <button
+              className={`${classes.btn} ${classes.blueBtn} ${classes.rounded}`}
+            >
+              {t("contact.sendBtn")}
+            </button>
+            <label>
+              {t("contact.policy")}
+              <a href="#">{t("contact.policyLink")}</a>
+            </label>
+            <span className={classes.attachment}>
+              <img src={attachment} alt="logo" /> {t("contact.file")}
+            </span>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h2 className={classes.h2}>{t("contact.sidePanel.headOffice")}</h2>
+        <p>10 Place Vendôme, 75001 Paris, France</p>
+        <br />
+        <h2 className={classes.h2}>{t("contact.sidePanel.contactUs")}</h2>
+        <p>+33 01 32 66 62 61</p>
+        <p>hello@newbrands.fr</p>
+      </div>
+    </div>
+  );
 }
 
 export default ContactContainer;
